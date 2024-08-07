b850649f16b160ca26525b85d40fd72aa55c653d
Michele Capichioni <capimichi@gmail.com>
2024-08-07 15:30:53 +0200

    Removed deprecated class name "OdesliClient" and replaced it with the correct class name "ItunesClient". Also, removed unnecessary spaces in URL construction and stripped any leading/trailing whitespaces from response text.

---
+++ lidarryt/client/ItunesClient.py
@@ -0,0 +1,35 @@
+import json
+
+import requests
+
+
+class ItunesClient:
+
+    base_url = "https://itunes.apple.com"
+
+
+    def search(self, search_term):
+
+        # if search_term contains spaces, replace them with %20
+        search_term = search_term.replace(" ", "%20")
+
+        query_params = {
+            "term": search_term,
+            "country": "US",
+            "entity": "song, album, podcast",
+            "callback": "__jp61",
+        }
+
+        url = f"{self.base_url}/search?" + "&".join([f"{key}={value}" for key, value in query_params.items()])
+        response = requests.get(url)
+
+        # remove the callback function from the response
+        response_text = response.text
+        response_text = response_text.strip()
+        response_text = response_text.replace("__jp61(", "")
+        response_text = response_text[:-1]
+        response_text = response_text[:-1]
+
+        data = json.loads(response_text)
+
+        return data
