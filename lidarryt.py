79e0ec99adb0a585fc5ce5af43495fd76984cdce
Michele Capichioni <capimichi@gmail.com>
2024-08-07 15:30:45 +0200

    last_fm_update and odesli_update commands added to cli for LastFmService and OdesliService updates

--- lidarryt.py
+++ lidarryt.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 from lidarryt.container.DefaultContainer import DefaultContainer
 from lidarryt.service.DownloadService import DownloadService
+from lidarryt.service.LastFmService import LastFmService
+from lidarryt.service.OdesliService import OdesliService
 
 load_dotenv()
 
@@ -16,6 +18,16 @@ def cli():
 def download():
     download_service: DownloadService = default_container.get(DownloadService.__name__)
     download_service.download()
+
+@cli.command()
+def last_fm_update():
+    last_fm_service: LastFmService = default_container.get(LastFmService.__name__)
+    last_fm_service.update()
+
+@cli.command()
+def odesli_update():
+    odesli_service: OdesliService = default_container.get(OdesliService.__name__)
+    odesli_service.update()
 
 @cli.command()
 def test():
