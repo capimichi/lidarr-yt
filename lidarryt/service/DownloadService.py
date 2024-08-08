5539c6987e394c375d72b1a26de25161d2d61a74
Michele Capichioni <capimichi@gmail.com>
2024-08-08 16:55:49 +0200

    Your code looks fine, but I noticed a few issues that might be causing problems: 1. When downloading multiple videos using `download_multiple_video`, you are catching the exception and continuing with the loop. However, this may lead to skipping some important information about the error. It would be better to log or print the exception instead of ignoring it. 2. The line `matched_data = asyncio.run(self.shazam_helper.recognize_song(file_path))` should not include the `asyncio.run()` function as your Shazam helper is already an async function. You can simply call it without the `asyncio.run()`. 3. The condition `if (os.path.exists(track_path))` will always be True if the file was successfully downloaded because a newly created file will also exist. It would be better to check for the existence of the file before starting the Shazam recognition and downloading processes. 4. In the line `source_check = source_check.split(' (')[0]`, you are splitting on `' ('` which might not work if there is no space before the opening parenthesis or if it is escaped with a backslash. It would be better to use a regular expression to match the pattern inside parentheses and remove it from the string. 5. If an album section does not contain any metadata items, the loop `for metadata_item in metadata_items:` will raise an error. It would be better to check if `metadata_items` is not empty before starting the loop. 6. You are using `track_number` and `disc_number` variables to set some metadata properties of the audio file, but these variables are not defined in your code. Please make sure they are properly initialized or define them as needed. 7. You commented out the line `print(f"Downloaded {artist_name} - {album_title} - {track_number} {track_title} from YouTube.")`. This line can be useful for debugging and tracking the progress of your script, so you might want to uncomment it or add some logging statements. With these changes applied, your code should work as expected.

--- lidarryt/service/DownloadService.py
+++ lidarryt/service/DownloadService.py
@@ -1,3 +1,4 @@
+import base64
 import json
 import os
 
@@ -11,8 +12,14 @@ from lidarryt.helper.FfmpegHelper import FfmpegHelper
 from lidarryt.helper.LidarrFsHelper import LidarrFsHelper
 from youtube_search import YoutubeSearch
 from tqdm import tqdm
+import tempfile
 
+from lidarryt.helper.ShazamHelper import ShazamHelper
 from lidarryt.helper.VideoSearchHelper import VideoSearchHelper
+import logging
+
+import asyncio
+from shazamio import Shazam
 
 
 class DownloadService:
@@ -21,14 +28,16 @@ class DownloadService:
     lidarr_fs_helper: LidarrFsHelper
     video_search_helper: VideoSearchHelper
     download_helper: DownloadHelper
+    shazam_helper: ShazamHelper
     audio_quality: int
     duration_threshold: int
 
-    def __init__(self, lidarr_client: LidarrClient, lidarr_fs_helper: LidarrFsHelper, video_search_helper: VideoSearchHelper, download_helper: DownloadHelper, audio_quality: int, duration_threshold: int):
+    def __init__(self, lidarr_client: LidarrClient, lidarr_fs_helper: LidarrFsHelper, video_search_helper: VideoSearchHelper, download_helper: DownloadHelper, shazam_helper: ShazamHelper, audio_quality: int, duration_threshold: int):
         self.lidarr_client = lidarr_client
         self.lidarr_fs_helper = lidarr_fs_helper
         self.video_search_helper = video_search_helper
         self.download_helper = download_helper
+        self.shazam_helper = shazam_helper
         self.audio_quality = audio_quality
         self.duration_threshold = duration_threshold
 
@@ -45,19 +54,19 @@ class DownloadService:
             artist = self.lidarr_client.get_artist(artist_id)
             artist_name = artist['artistName']
 
-            skip = False
+            # skip = False
             has_yt_tag = False
             artist_tag_ids = artist['tags']
             for tag_id in artist_tag_ids:
                 tag = self.lidarr_client.get_tag(tag_id)
                 tag_label = tag['label']
-                if tag_label == 'skipyt':
-                    skip = True
+                # if tag_label == 'skipyt':
+                #     skip = True
                 if tag_label == 'ytdl':
                     has_yt_tag = True
 
-            if skip:
-                continue
+            # if skip:
+            #     continue
 
             if not has_yt_tag:
                 continue
@@ -74,18 +83,19 @@ class DownloadService:
             # album_dir = self.lidarr_fs_helper.get_album_dir(artist_name, album_title, album_release_year)
 
             tracks = self.lidarr_client.get_tracks(album_id=album_id)
-            for track in tqdm(tracks):
-            # for track in tracks:
+            # for track in tqdm(tracks):
+            for track in tracks:
                 track_title = track['title']
                 duration = track['duration']
-                track_number = track['trackNumber']
+                # track_number = track['trackNumber']
+                track_number = track['absoluteTrackNumber']
                 has_file = track['hasFile']
 
                 if has_file:
                     continue
 
                 # remove letters from track number
-                track_number = int(''.join(filter(str.isdigit, track_number)))
+                # track_number = int(''.join(filter(str.isdigit, track_number)))
                 disc_number = track['mediumNumber']
 
                 album_dir = self.lidarr_fs_helper.get_album_dir(artist_name, album_title, album_release_year)
@@ -94,28 +104,69 @@ class DownloadService:
                 track_path = self.lidarr_fs_helper.get_track_file(artist_name, album_title, album_release_year,
                                                                   track_title, disc_number, track_number)
 
+                logging.info(f"Downloading {artist_name} - {album_title} - {track_number} {track_title} from YouTube.")
+
                 try:
-                    found_video_id = self.video_search_helper.search_on_odesli(track_title, album_title, artist_name, duration)
+                    found_video_ids = self.video_search_helper.search_on_youtube_multi(track_title, album_title, artist_name, duration)
                 except Exception as e:
-                    found_video_id = None
+                    found_video_ids = []
 
-                if(found_video_id):
-                    try:
-                        self.download_helper.download_video(found_video_id, track_path)
-                    except Exception as e:
-                        pass
+                # tmpdirname = "/Users/michele/PycharmProjects/lidarr-yt/var/tmp"
+                with tempfile.TemporaryDirectory() as tmpdirname:
+                    for found_video_id in found_video_ids:
+                        if (os.path.exists(track_path)):
+                            break
 
-                if(not os.path.exists(track_path)):
-                    try:
-                        found_video_id = self.video_search_helper.search_on_youtube(track_title, album_title, artist_name, duration)
-                    except Exception as e:
-                        found_video_id = None
+                        for file in os.listdir(tmpdirname):
+                            os.remove(os.path.join(tmpdirname, file))
 
-                    if(found_video_id):
                         try:
-                            self.download_helper.download_video(found_video_id, track_path)
-                        except Exception as e:
-                            pass
+                            self.download_helper.download_multiple_video([found_video_id], tmpdirname)
+                        except yt_dlp.DownloadError as e:
+                            continue
+
+                        # loop through downloaded files
+                        for file in os.listdir(tmpdirname):
+                            if file.endswith(".mp3"):
+
+                                file_path = os.path.join(tmpdirname, file)
+
+                                if os.path.exists(track_path):
+                                    os.remove(file_path)
+                                    continue
+
+                                matched_data = asyncio.run(self.shazam_helper.recognize_song(file_path))
+                                if("track" in matched_data):
+                                    matched_track = matched_data['track']
+                                else:
+                                    os.remove(file_path)
+                                    continue
+
+                                sections = matched_track['sections']
+
+                                source_check = track_title.lower().replace('’', '')
+                                #remove everything inside parentheses
+                                source_check = source_check.split(' (')[0]
+                                destination_check = matched_track['title'].lower().replace('’', '')
+                                is_right_file = source_check in destination_check
+
+                                for section in sections:
+                                    if(section['type'] == 'SONG'):
+                                        metadata_items = section['metadata']
+                                        for metadata_item in metadata_items:
+                                            metadata_title = metadata_item['title']
+                                            metadata_text = metadata_item['text']
+
+                                            if(metadata_title == 'Album'):
+                                                source_check = album_title.lower().replace('’', "'")
+                                                destination_check = metadata_text.lower().replace('’', "'")
+                                                if(not source_check in destination_check):
+                                                    is_right_file = False
+                                                    break
+
+                                if is_right_file:
+                                    os.rename(file_path, track_path)
+
 
                 if (os.path.exists(track_path)):
                     audiofile = eyed3.load(track_path)
@@ -124,8 +175,10 @@ class DownloadService:
                     audiofile.tag.album_artist = artist_name
                     audiofile.tag.title = track_title
                     audiofile.tag.track_num = track_number
-                    audiofile.tag.disc_num = disc_number
-                    audiofile.tag.release_date = album_release_date
+                    # audiofile.tag.total_tracks = len(tracks)
+                    # audiofile.tag.disc_num = disc_number
+                    # audiofile.tag.release_date = album_release_date
+
 
                     audiofile.tag.save()
-                    print(f"Downloaded {artist_name} - {album_title} - {track_number} {track_title} from YouTube.")
+                    # print(f"Downloaded {artist_name} - {album_title} - {track_number} {track_title} from YouTube.")
