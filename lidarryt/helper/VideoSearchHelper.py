55bc09a5f010eb212205b684b5f4d685d922a86e
Michele Capichioni <capimichi@gmail.com>
2024-08-08 16:54:55 +0200

    Added support for multiple YouTube video IDs and improved sorting logic in VideoSearchHelper.py

--- lidarryt/helper/VideoSearchHelper.py
+++ lidarryt/helper/VideoSearchHelper.py
@@ -11,13 +11,13 @@ class VideoSearchHelper:
         self.youtube_duration_threshold = youtube_duration_threshold
 
 
-    def search_on_youtube(self, track_title, album_title, artist_name, duration):
+    def search_on_youtube_multi(self, track_title, album_title, artist_name, duration):
         search_terms = [
-            f"{track_title} - {album_title} - {artist_name}",
             f"{track_title} {artist_name}",
+            f"{track_title} - {album_title} - {artist_name}",
         ]
 
-        found_video_id = None
+        found_video_ids = []
         for search_term in search_terms:
 
             youtube_results = YoutubeSearch(search_term, max_results=10)
@@ -26,24 +26,54 @@ class VideoSearchHelper:
                 video_title = video['title']
 
                 # avoid live versions
-                if 'live' in video_title.lower():
-                    continue
+                # if ' live' in video_title.lower():
+                #     continue
 
                 video_duration_mm_ss = video['duration']
                 video_duration_mm = int(video_duration_mm_ss.split(':')[0])
                 video_duration_ss = int(video_duration_mm_ss.split(':')[1])
                 video_duration_ms = (video_duration_mm * 60 + video_duration_ss) * 1000
 
+                # skip if video duration is more than 20 minutes
+                if video_duration_mm > 10:
+                    continue
+
                 duration_difference = abs(duration - video_duration_ms) / 1000
+                # if (video_duration_ms <= 0 or duration_difference <= self.youtube_duration_threshold):
+                found_video_ids.append({
+                    'id': video_id,
+                    'title': video_title,
+                    'duration_difference': duration_difference
+                })
 
-                if (duration_difference <= self.youtube_duration_threshold):
-                    found_video_id = video_id
-                    break
+        # if(duration > 0):
+        #     found_video_ids = sorted(found_video_ids, key=lambda x: x['duration_difference'])
+        def sort(x):
+            title = x['title'].lower()
+            # duration_difference = x['duration_difference']
+            has_keywords = ('lyric' in title) or ('official' in title)
+            has_keywords_value = 0 if has_keywords else 1
+            return has_keywords_value
+        found_video_ids = sorted(found_video_ids, key=sort)
 
-        if not found_video_id:
-            return None
+        found_video_ids = [video['id'] for video in found_video_ids]
 
-        return found_video_id
+        unique_ids = []
+        for video_id in found_video_ids:
+            if video_id not in unique_ids:
+                unique_ids.append(video_id)
+        found_video_ids = unique_ids
+
+        # make unique
+        # found_video_ids = list(set(found_video_ids))
+
+        return found_video_ids
+
+    def search_on_youtube(self, track_title, album_title, artist_name, duration):
+        ids = self.search_on_youtube_multi(track_title, album_title, artist_name, duration)
+        if len(ids) > 0:
+            return ids[0]
+        return None
 
     def search_on_odesli(self, track_title, album_title, artist_name, duration):
         search_term = f"{track_title} - {album_title} - {artist_name}"
