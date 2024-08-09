dbb4212244e36fec74d4902f00d229f96b8c32d1
Michele Capichioni <capimichi@gmail.com>
2024-08-09 15:08:24 +0200

    Updated LidarrFsHelper class to include preferred codec and added methods for getting Lidarr album directory and track file path

--- lidarryt/helper/LidarrFsHelper.py
+++ lidarryt/helper/LidarrFsHelper.py
@@ -1,10 +1,15 @@
 import os
+from datetime import datetime
+
+from injector import inject
 
 
 class LidarrFsHelper:
 
     root_folder : str = None
+    preferred_codec : str = "mp3"
 
+    @inject
     def __init__(self, root_folder):
         self.root_folder = root_folder
 
@@ -23,3 +28,31 @@ class LidarrFsHelper:
         parsed_album_title = album_title.replace("/", " ")
         parsed_track_title = track_title.replace("/", " ")
         return os.path.join(self.get_album_dir(artist_name, album_title, album_release_year), f"{parsed_artist_name} - {parsed_album_title} - {parsed_track_number} {parsed_track_title}.mp3")
+
+    def get_lidarr_album_dir(self, album):
+        parsed_date = datetime.fromisoformat(album["releaseDate"].replace("Z", "+00:00"))
+        album_year = parsed_date.year
+        album_name = album["title"]
+        album_folder = f"{album_name} ({album_year})"
+        album_dir = os.path.join(album["artist"]["path"], album_folder)
+
+        # tmp for local env
+        album_dir = "/Users/michele/navidrome" + album_dir
+
+        return album_dir
+
+
+
+    def get_lidarr_track_file(self, album, track):
+        album_dir = self.get_lidarr_album_dir(album)
+        artist_str = os.path.basename(album["artist"]['artistName'])
+        album_name = album["title"]
+        title = track["title"]
+        title_str = title
+        track_number = str(track["absoluteTrackNumber"]).zfill(2)
+
+        filename = f"{artist_str} - {album_name} - {track_number} - {title_str}.{self.preferred_codec}"
+        full_file_path = os.path.join(album_dir, filename)
+
+        return full_file_path
+
