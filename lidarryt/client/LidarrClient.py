38b2a0a0c2c6dfe9bf6d9103453c9eed35ee5a45
Michele Capichioni <capimichi@gmail.com>
2024-08-09 15:07:54 +0200

    Added new methods for getting track files, importing songs, and retrieving remote paths. Also updated LidarrClient initialization with dependency injection using Injector library.

--- lidarryt/client/LidarrClient.py
+++ lidarryt/client/LidarrClient.py
@@ -1,7 +1,10 @@
 import requests
+from injector import inject
 
 
 class LidarrClient:
+
+    @inject
     def __init__(self, base_url, api_key):
         self.base_url = base_url
         self.api_key = api_key
@@ -34,6 +37,48 @@ class LidarrClient:
 
     def get_tag(self, tag_id):
         url = f"{self.base_url}/api/v1/tag/{tag_id}"
+        response = requests.get(url, headers={"X-Api-Key": self.api_key})
+        data = response.json()
+        return data
+
+    def get_trackfiles(self, artist_id, album_id):
+        url = f"{self.base_url}/api/v1/trackfile"
+        params = {
+            "artistId": artist_id,
+            "albumId": album_id
+        }
+        url += "?" + "&".join([f"{k}={v}" for k, v in params.items()])
+        response = requests.get(url, headers={"X-Api-Key": self.api_key})
+        data = response.json()
+        return data
+
+    def import_song(self, song, req_album, filename):
+        self.general_logger.warning(f"Attempting import of song via Lidarr API")
+        endpoint = f"{self.base_url}/api/v1/manualimport"
+        headers = {"X-Api-Key": self.api_key, "Content-Type": "application/json"}
+        full_file_path = os.path.join(req_album["album_full_path"], filename)
+        data = {
+            "id": song["track_id"],
+            "path": full_file_path,
+            "name": song["track_title"],
+            "artistId": req_album["artist_id"],
+            "albumId": req_album["album_id"],
+            "albumReleaseId": req_album["album_release_id"],
+            "quality": {},
+            "releaseGroup": "",
+            "indexerFlags": 0,
+            "downloadId": "",
+            "additionalFile": False,
+            "replaceExistingFiles": False,
+            "disableReleaseSwitching": False,
+            "rejections": [],
+        }
+        response = requests.post(endpoint, json=[data], headers=headers)
+        return response.json()
+
+    def get_remote_path(self, id):
+        id = str(id)
+        url = f"{self.base_url}/api/v1/track/{id}"
         response = requests.get(url, headers={"X-Api-Key": self.api_key})
         data = response.json()
         return data
