d42e97435c571e1dba7c8c90c11fa3bb3b8fe0bc
Michele Capichioni <capimichi@gmail.com>
2024-08-09 09:23:45 +0200

    Added new method get_track_apple_music_url to retrieve Apple Music URL for a track using the OdesliClient class.

--- lidarryt/client/OdesliClient.py
+++ lidarryt/client/OdesliClient.py
@@ -22,3 +22,19 @@ class OdesliClient:
                 break
 
         return youtube_id
+
+    def get_track_apple_music_url(self, id):
+        str_id = str(id)
+        url = f"{self.base_url}/i/{str_id}"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        links = soup.find_all('a')
+
+        apple_music_url = None
+        for link in links:
+            if 'geo.music.apple.com' in link['href']:
+                apple_music_url = link['href']
+                break
+
+        return apple_music_url
