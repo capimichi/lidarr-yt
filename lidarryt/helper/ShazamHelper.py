7badec43b6d44b018f743b2914c0d2e1008d1b9a
Michele Capichioni <capimichi@gmail.com>
2024-08-10 00:50:24 +0200

    Refactored ShazamHelper class to use AudioHelper, added inject and modified recognize_song method.

--- lidarryt/helper/ShazamHelper.py
+++ lidarryt/helper/ShazamHelper.py
@@ -1,9 +1,24 @@
+import os
+import tempfile
+from typing import Optional
+
+import requests
+from injector import inject
 from shazamio import Shazam
+
+from lidarryt.helper.AudioHelper import AudioHelper
+from lidarryt.model.ShazamData import ShazamData
 
 
 class ShazamHelper:
 
+    audio_helper: AudioHelper
+
+    @inject
+    def __init__(self, audio_helper: AudioHelper):
+        self.audio_helper = audio_helper
+
     async def recognize_song(self, audio_file_path):
         shazam = Shazam()
-        out = await shazam.recognize(audio_file_path)
+        out = await shazam.recognize_song(audio_file_path)
         return out
