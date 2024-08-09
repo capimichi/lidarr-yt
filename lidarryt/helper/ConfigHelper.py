f221563c29f2fcdfcf4e2994f15a682b42378cc0
Michele Capichioni <capimichi@gmail.com>
2024-08-09 15:08:09 +0200

    Refactored DownloadHelper to ConfigHelper and updated audio quality initialization.

---
+++ lidarryt/helper/ConfigHelper.py
@@ -0,0 +1,60 @@
+import os
+
+import eyed3
+import yt_dlp
+from injector import inject
+from youtube_search import YoutubeSearch
+
+
+class ConfigHelper:
+
+    audio_quality = 5
+
+    @inject
+    def __init__(self):
+        self.audio_quality = os.getenv('YOUTUBE_AUDIO_QUALITY', 5)
+
+    def download_multiple_video(self, ids, dir):
+        urls = [f"https://www.youtube.com/watch?v={id}" for id in ids]
+        track_path_template = "%(id)s.%(ext)s"
+        track_path_template = os.path.join(dir, track_path_template)
+        options = {
+            'extract_audio': True,
+            'audio_format': 'mp3',
+            'audio_quality': self.audio_quality,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': str(self.audio_quality),
+            }],
+            'format': 'bestaudio',
+            # 'quiet': True,
+            # 'noprogress': True,
+            'outtmpl': track_path_template,
+        }
+        with yt_dlp.YoutubeDL(options) as ydl:
+            ydl.download(
+                urls
+            )
+
+    def download_video(self, id, path):
+        track_path_template = path.replace('.mp3', '.%(ext)s')
+
+        video_url = f"https://www.youtube.com/watch?v={id}"
+        options = {
+            'extract_audio': True,
+            'audio_format': 'mp3',
+            'audio_quality': self.audio_quality,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': str(self.audio_quality),
+            }],
+            'format': 'bestaudio',
+            # 'quiet': True,
+            # 'noprogress': True,
+            'outtmpl': track_path_template,
+        }
+        with yt_dlp.YoutubeDL(options) as ydl:
+            ydl.download([video_url])
+
