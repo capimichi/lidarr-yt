6f3d69c96bd18283ab5186f5124bc85ac26744f0
Michele Capichioni <capimichi@gmail.com>
2024-08-10 14:51:30 +0200

    Added typing import for Optional, updated get_album_data method to include type hints and return optional AppleAlbumData object.

--- lidarryt/client/AppleMusicClient.py
+++ lidarryt/client/AppleMusicClient.py
@@ -1,13 +1,17 @@
 import json
+from typing import Optional
+
 from bs4 import BeautifulSoup
 import requests
+
+from lidarryt.model.AppleAlbumData import AppleAlbumData
 
 
 class AppleMusicClient:
 
     base_url = "https://music.apple.com"
 
-    def get_album_data(self, id):
+    def get_album_data(self, id) -> Optional[AppleAlbumData]:
         str_id = str(id)
         url = f"{self.base_url}/sm/album/_/" + str_id
         response = requests.get(url)
@@ -16,9 +20,10 @@ class AppleMusicClient:
         scripts = soup.find_all('script')
         for script in scripts:
             script_text = script.text
-            if 'MusicAlbum' in script_text:
+            if 'AlbumDetailPageIntent' in script_text:
                 data = json.loads(script_text)
-                if("tracks" in data):
-                    return data
+                if(len(data) > 0 and "intent" in data[0]):
+                    album_data = AppleAlbumData(data[0])
+                    return album_data
 
         return None
