f9604c4acb5bad639a9fb8cc1e55e6da976e9471
Michele Capichioni <capimichi@gmail.com>
2024-08-10 16:38:33 +0200

    Added new services and implemented logging for the cron job.

---
+++ cron.py
@@ -0,0 +1,43 @@
+import fasteners
+import schedule
+import time
+import os
+
+import click
+from dotenv import load_dotenv
+
+from lidarryt.container.DefaultContainer import DefaultContainer
+from lidarryt.service.DownloadService import DownloadService
+from lidarryt.service.LastFmService import LastFmService
+from lidarryt.service.OdesliService import OdesliService
+
+load_dotenv()
+
+import logging
+
+if(not os.path.exists("var/log")):
+    os.makedirs("var/log")
+logging.basicConfig(
+    filename="var/log/app.log",
+    encoding="utf-8",
+    filemode="a",
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M",
+)
+
+
+default_container = DefaultContainer.getInstance()
+
+def download_job():
+    lock = fasteners.InterProcessLock("download.lock")
+    lock.acquire()
+    download_service: DownloadService = default_container.get(DownloadService)
+    download_service.download()
+
+schedule.every().minute.do(download_job)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
