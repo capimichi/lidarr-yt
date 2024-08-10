4521c86799e1a1db259a6e496d06684df3782c81
Michele Capichioni <capimichi@gmail.com>
2024-08-10 14:52:40 +0200

    Add disc number to track file name and update function signature

--- lidarryt/helper/LidarrFsHelper.py
+++ lidarryt/helper/LidarrFsHelper.py
@@ -44,15 +44,16 @@ class LidarrFsHelper:
 
 
 
-    def get_lidarr_track_file(self, album, track_title, track_number):
+    def get_lidarr_track_file(self, album, track_title, track_number, disc_number):
         album_dir = self.get_lidarr_album_dir(album)
         artist_str = os.path.basename(album["artist"]['artistName'])
         album_name = album["title"]
         album_str = album_name.replace("/", " ")
         title_str = track_title.replace("/", " ")
         track_number = str(track_number).zfill(2)
+        disc_number = str(disc_number).zfill(2)
 
-        filename = f"{artist_str} - {album_str} -  - {track_number} - {title_str}.{self.preferred_codec}"
+        filename = f"{artist_str} - {album_str} - {disc_number} - {track_number} - {title_str}.{self.preferred_codec}"
         full_file_path = os.path.join(album_dir, filename)
 
         return full_file_path
