6b292a1ded12f67f65a5e898018ef959250294eb
Michele Capichioni <capimichi@gmail.com>
2024-08-11 21:14:24 +0200

    Update: Added option to enable proxy for YT-DLP downloads and adjusted __init__ method

--- lidarryt/helper/DownloadHelper.py
+++ lidarryt/helper/DownloadHelper.py
@@ -4,20 +4,24 @@ import eyed3
 import yt_dlp
 from injector import inject
 from youtube_search import YoutubeSearch
+from fp.fp import FreeProxy
 
 
 class DownloadHelper:
 
     audio_quality = 5
+    enable_proxy = False
 
     @inject
-    def __init__(self, audio_quality):
+    def __init__(self, audio_quality, enable_proxy):
         self.audio_quality = audio_quality
+        self.enable_proxy = enable_proxy
 
     def download_multiple_video(self, ids, dir):
         urls = [f"https://www.youtube.com/watch?v={id}" for id in ids]
         track_path_template = "%(timestamp)s.%(ext)s"
         track_path_template = os.path.join(dir, track_path_template)
+
         options = {
             'extract_audio': True,
             'audio_format': 'mp3',
@@ -32,6 +36,9 @@ class DownloadHelper:
             # 'noprogress': True,
             'outtmpl': track_path_template,
         }
+        if(self.enable_proxy):
+            options['proxy'] = FreeProxy(rand=True, anonym=True, timeout=5).get()
+
         with yt_dlp.YoutubeDL(options) as ydl:
             ydl.download(
                 urls
