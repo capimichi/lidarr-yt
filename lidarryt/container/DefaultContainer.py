831a201bb2bbb66f0021d6cdcf088b7ed1abc23b
Michele Capichioni <capimichi@gmail.com>
2024-08-13 22:38:18 +0200

    Added ProxyHelper and updated DownloadHelper binding

--- lidarryt/container/DefaultContainer.py
+++ lidarryt/container/DefaultContainer.py
@@ -10,6 +10,7 @@ from lidarryt.client.OdesliClient import OdesliClient
 from lidarryt.helper.DownloadHelper import DownloadHelper
 from lidarryt.helper.FfmpegHelper import FfmpegHelper
 from lidarryt.helper.LidarrFsHelper import LidarrFsHelper
+from lidarryt.helper.ProxyHelper import ProxyHelper
 from lidarryt.helper.ShazamHelper import ShazamHelper
 from lidarryt.helper.VideoSearchHelper import VideoSearchHelper
 from lidarryt.service.DownloadService import DownloadService
@@ -37,15 +38,21 @@ class DefaultContainer:
 
         youtube_audio_quality = int(os.getenv('YOUTUBE_AUDIO_QUALITY'))
         enable_proxy = os.getenv('ENABLE_PROXY') == "true"
+        proxy = os.getenv('PROXY')
 
         last_fm_api_key = os.getenv('LASTFM_API_KEY')
 
         self._injector = Injector()
 
+
+
         self._injector.binder.bind(LidarrClient, LidarrClient(lidarr_base_url, lidarr_api_key))
         self._injector.binder.bind(LastFmClient, LastFmClient(last_fm_api_key))
+
+
+        self._injector.binder.bind(ProxyHelper, ProxyHelper(enable_proxy, proxy))
         self._injector.binder.bind(LidarrFsHelper, LidarrFsHelper(lidarr_root_folder))
-        self._injector.binder.bind(DownloadHelper, DownloadHelper(youtube_audio_quality, enable_proxy))
+        self._injector.binder.bind(DownloadHelper, DownloadHelper(youtube_audio_quality, self.get(ProxyHelper)))
 
 
 
